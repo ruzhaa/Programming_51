041b0f68383cd75e0fb929fd35065e4f14c72a3e
ruzhaa <ruzha_bobotanova@abv.bg>
2015-08-31 10:09:30 +0300

    Update CoffeeMachine.java

--- week2/Coffee-Machine/CoffeeMachine.java
+++ week2/Coffee-Machine/CoffeeMachine.java
@@ -50,5 +50,31 @@ public class CoffeeMachine {
 	public double getBalance() {
 		return balance;
 	}
+	
+	public static void main(String[] args) {
+		// Coffee.java
+		Coffee Java = new Coffee("Java", 1.00);
+		System.out.println(Java);
+		Java.toString();
+
+		// CoffeeMachine.java
+		CoffeeMachine cm = new CoffeeMachine();
+
+		Coffee java = new Coffee();
+		Coffee latte = new Coffee();
+
+		java.brand = "Java";
+		java.price = 1.00;
+
+		latte.brand = "Latte";
+		latte.price = 1.50;
+
+		cm.addCoffee(java);
+		cm.addCoffee(latte);
+
+		System.out.println(cm.buyCoffee(latte, 3.40));
+		System.out.println("Current balance: " + cm.getBalance());
+	}
+
 
 }
